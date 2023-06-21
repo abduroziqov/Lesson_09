9e5cb383caa5c062a5c858b0f599395a7b0df355
QUVONCHBEK ABDUROZIQOV <75139080+abduroziqov@users.noreply.github.com>
2023-06-21 17:13:24 +0500

    Add project files.

---
+++ Lesson_09/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Lesson_09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(() =>  DisplayNumbers());
+
+            thread.Start();
+        }
+        public static void DisplayNumbers()
+        {
+            Console.Write("Enter a number => ");
+
+            int counter = 1;
+
+            double inputNumber = double.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= inputNumber; i++)
+            {
+                Console.WriteLine($"Thread name: {counter++} => {i}");
+                Thread.Sleep(500);
+            }
+            Console.WriteLine("Thread is complete!");
+        }
+    }
+}
